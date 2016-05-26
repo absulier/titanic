281a08b719ce7d6f0baea45d299f4c986ad2c6a4
Adam Sulier <absulier@gmail.com>
2016-05-25 21:15:08 -0400

    initial push

---
+++ model.py
@@ -0,0 +1,65 @@
+import numpy as np
+import pandas as pd
+import matplotlib.pyplot as plt
+from patsy import dmatrices
+from sklearn.linear_model import LogisticRegression as lr
+from sklearn.cross_validation import train_test_split, cross_val_score
+from sklearn import metrics
+from sklearn.metrics import classification_report as skcr
+from sklearn.metrics import confusion_matrix as skcm
+import seaborn
+from sklearn.cross_validation import train_test_split as tts
+from sklearn.cross_validation import cross_val_score
+%matplotlib inline
+
+df=pd.read_csv('titanic.csv')
+
+#The assumptions of this data is that it is accurate and represents the entire population of the titanic.
+#There are less than 900 individuals listed in this data and there were almost 3000 onboard the titanic.
+#Therefor, we are taking a risk in assuming that this population is repesentative
+#and we are also taking a risk in assuming that these old records are accurate.
+df.head()
+
+df.isnull().sum()
+df.Parch.unique()
+
+df=df.drop(['Cabin','Ticket','Name','index','PassengerId'], axis=1)
+df=df.dropna()
+
+df.describe()
+
+df.dtypes
+df.Age.sort_values()
+
+df=pd.get_dummies(df)
+df.dtypes
+
+y=df.Survived
+x=df.drop('Survived',axis=1)
+
+#makes a baseline for accuracy
+float(len(y[y==0]))/float(len(y))
+
+model=lr()
+model.fit(x,y)
+model.score(x,y)
+
+for column in x.columns:
+    print column, np.corrcoef(x[column],y)[1][0]
+
+x_train, x_test, y_train, y_test = tts(x,y, train_size=.8, random_state=1)
+
+#train/test fitting and validation
+model.score(x_test,y_test)
+proba=model.predict_proba(x_test)
+pred=model.predict(x_test)
+
+s= cross_val_score(model,x_test,y_test, cv=5)
+s.mean()
+s.std()
+
+#The f-1 scores show that our model does a fairly decent job of predicting those
+#who died and an okay job predicting those who survived
+print skcr(y_test,pred)
+
+print skcm(y_test,pred)
